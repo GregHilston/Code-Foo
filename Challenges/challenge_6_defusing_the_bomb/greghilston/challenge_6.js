3662ff445905958cfb3296d17e4eb38c4a35fea9
GregHilston <Gregory.Hilston@gmail.com>
2018-10-05 15:43:33 -0600

    Got program working, need to write unit tests

---
+++ Challenges/challenge_6_defusing_the_bomb/greghilston/challenge_6.js
@@ -0,0 +1,48 @@
+"use strict" // forcing ourselves to write better NodeJS
+
+const util = require('util')        // used to print objects
+const assert = require('assert')    // used for testing
+
+// our finite state machine defining valid moves
+const fsm = {"start": ["white, red, black, orange, green, purple"],
+            "white": ["red", "orange", "green", "purple"],
+            "red": ["green"],
+            "black": ["red", "black", "purple"], 
+            "orange": ["red", "black"],
+            "green": ["white", "orange"],
+            "purple": ["red", "black"]}
+const validWires = [Object.keys(fsm)] // for convenience
+const expectedNumberOfArguments = 3 // first for invoked node path, second for script path, third for multiline string 
+
+function checkWireCutting(wires) {
+    var lastWire = null
+
+    wires = wires.split('\n')
+    for(var i = 0; i < wires.length; i++) {    
+        var wire = wires[i].trim()
+
+        // check if wire is a valid wire
+        if(validWires.indexOf(wire) > -1) {
+            throw new Error("Unexpected wire: '" + wire + "'")
+        }
+
+        // check if this step to this wire is valid
+        if(lastWire != null) {            
+            if(fsm[lastWire].indexOf(wire) <= -1) {
+                console.log("Boom")
+                return null
+            }
+        }
+
+        lastWire = wire
+    }
+
+    console.log("Bomb defused")
+}
+
+if (process.argv.length != expectedNumberOfArguments) {
+    throw new Error("Unexpected number of input arguments. Expected " + expectedNumberOfArguments + " actual " + process.argv.length)
+}
+
+var wires = process.argv[2]
+checkWireCutting(wires)
