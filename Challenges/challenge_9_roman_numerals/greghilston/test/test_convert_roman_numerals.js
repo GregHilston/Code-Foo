33dbc3fd10d281991b121ace2692106faa22aad0
GregHilston <Gregory.Hilston@gmail.com>
2018-10-16 12:29:20 -0600

    Got rid of unnecessary defined var, and changed var to let

--- Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
+++ Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
@@ -7,11 +7,10 @@ describe('convertRomanNumerals', function() {
         inputAndOutput = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
                           "XI": 11, "XII": 12, "XIII": 13, "XIV": 14, "XV": 15, "XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20,
                           "XXI": 21, "XXII": 22, "XXIII": 23, "XXIV": 24, "XXV": 25, "XXVI": 26, "XXVII": 27, "XXVIII": 28, "XXIX": 29, "XXX": 30}
-        let returnedValue
 
         // when
-        for (var input in inputAndOutput) {
-            returnedValue = convertRomanNumerals(input) 
+        for (let input in inputAndOutput) {
+            let returnedValue = convertRomanNumerals(input) 
 
             // then
             chai.assert.isTrue(inputAndOutput[input] == returnedValue, "should be correct value")
