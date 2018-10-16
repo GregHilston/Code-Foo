ebb553f9b1faf6a12f66194ead1bab5e96fb5553
GregHilston <Gregory.Hilston@gmail.com>
2018-10-16 12:27:14 -0600

    removed unnecessary parts of my test, thanks to thomas' help

--- Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
+++ Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
@@ -8,21 +8,12 @@ describe('convertRomanNumerals', function() {
                           "XI": 11, "XII": 12, "XIII": 13, "XIV": 14, "XV": 15, "XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20,
                           "XXI": 21, "XXII": 22, "XXIII": 23, "XXIV": 24, "XXV": 25, "XXVI": 26, "XXVII": 27, "XXVIII": 28, "XXIX": 29, "XXX": 30}
         let returnedValue
-        let caughtException
 
         // when
         for (var input in inputAndOutput) {
-            try {
-                returnedValue = convertRomanNumerals(input) 
-            } catch(exception) {
-                caughtException = exception
-            }
+            returnedValue = convertRomanNumerals(input) 
 
             // then
-            chai.assert.isDefined(returnedValue, "a value should be returned")
-            chai.assert.isUndefined(caughtException, "should be undefined, as nothing should have been thrown")
-            console.log("expected: " + inputAndOutput[input])
-            console.log("actual: " + returnedValue + "\n")
             chai.assert.isTrue(inputAndOutput[input] == returnedValue, "should be correct value")
         }
     })
