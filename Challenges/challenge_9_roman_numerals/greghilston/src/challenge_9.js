94da3bc683d0f460a40f1d06291235a8ec7a1ee9
GregHilston <Gregory.Hilston@gmail.com>
2018-10-17 07:52:59 -0600

    Added readme

--- Challenges/challenge_9_roman_numerals/greghilston/src/challenge_9.js
+++ Challenges/challenge_9_roman_numerals/greghilston/src/challenge_9.js
@@ -3,10 +3,19 @@
 const expectedNumberOfArguments = 3 // first for invoked node path, second for script path, third for multiline string 
 let mappings = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
 
+/**
+ * Converts a string roman numeral value to an int value
+ * 
+ * @param {string} romanNumeralWritten 
+ */
 function convertRomanNumerals(romanNumeralWritten){
     let runningSum = 0
 
     for(var i = 0; i < romanNumeralWritten.length; i++) {
+        if(!(romanNumeralWritten[i] in mappings)) {
+            return undefined // an unexpected character was found
+        }
+
         if((i == romanNumeralWritten.length - 1) || (mappings[romanNumeralWritten[i]] >= mappings[romanNumeralWritten[i + 1]])) {
             runningSum += mappings[romanNumeralWritten[i]]
         } else {
--- Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
+++ Challenges/challenge_9_roman_numerals/greghilston/test/test_convert_roman_numerals.js
@@ -3,18 +3,31 @@ const convertRomanNumerals = require('../src/challenge_9')
 const expect = chai.expect
 
 describe('convertRomanNumerals', function() {
-    it("'1 Through 100", function() {
+    it("'Valid Input (1 Through 100)", function() {
         // given
-        inputAndOutput = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
+        validInputAndOutput = {"I": 1, "II": 2, "III": 3, "IV": 4, "V": 5, "VI": 6, "VII": 7, "VIII": 8, "IX": 9, "X": 10,
                           "XI": 11, "XII": 12, "XIII": 13, "XIV": 14, "XV": 15, "XVI": 16, "XVII": 17, "XVIII": 18, "XIX": 19, "XX": 20,
                           "XXI": 21, "XXII": 22, "XXIII": 23, "XXIV": 24, "XXV": 25, "XXVI": 26, "XXVII": 27, "XXVIII": 28, "XXIX": 29, "XXX": 30}
 
         // when
-        for (let input in inputAndOutput) {
+        for (let input in validInputAndOutput) {
             const returnedValue = convertRomanNumerals(input) 
 
             // then
-            expect(inputAndOutput[input]).to.equal(returnedValue)
+            expect(validInputAndOutput[input]).to.equal(returnedValue)
+        }
+    })
+
+    it("'Invalid Input Handled Appropriately", function() {
+        // given
+        validInputAndOutput = {"ABC": undefined, "XLXX": undefined}
+
+        // when
+        for (let input in validInputAndOutput) {
+            const returnedValue = convertRomanNumerals(input) 
+
+            // then
+            expect(validInputAndOutput[input]).to.equal(returnedValue)
         }
     })
 })
