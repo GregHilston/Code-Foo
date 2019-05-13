a5982418a7bc29237eb6d8b09824c9cdaacafb5b
TS000 <tylersnell@me.com>
2019-05-13 12:05:06 -0600

    more beer

--- Challenges/challenge_36_socode/socode.js
+++ Challenges/challenge_36_socode/socode.js
@@ -1,36 +1,35 @@
-/**
- * Returns the answer to life, the universe and everything.
- */
-function soCode() {
-    console.log("Hello World!")
+  /**
+   * Returns the answer to life, the universe and everything.
+   */
+  function soCode() {
+    console.log("Hello World!");
 
-    return 42
-}
+    return 42;
+  }
 
-/**
- * Wraps up our social code experiment
- */
-function codeFoo() {
-    console.log("Goodbye Cruel World!")
-}
+  /**
+   * Wraps up our social code experiment
+   */
+  function codeFoo() {
+    console.log("Goodbye Cruel World!");
+  }
 
-// BEGIN section to add your functions
-//find the beer
-const TS000 = () => {
-    const url = "https://api.openbrewerydb.org/breweries/search?query=fort-collins";
+  // BEGIN section to add your functions
+  //find the beer
+  function TS000(query, amount) {
+    const url = `https://api.openbrewerydb.org/breweries/search?query=${query}&per_page=${amount}`;
     fetch(url) //find some beer!
       .then(response => response.json())
       .then(json => console.log(json))
       .catch(function(error) {
         console.log(error);
       });
-    }
-    
-    TS000()
+  }
 
-// END section to add your functions
+  // END section to add your functions
 
-// BEGIN section to call your functions
-let seed = soCode();
-// END section to call your functions
-codeFoo()
+  // BEGIN section to call your functions
+  let seed = soCode();
+  TS000("fort-collins", seed);
+  // END section to call your functions
+  codeFoo();
