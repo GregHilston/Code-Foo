9793d9ea8f7b16c7d6c183f42e253d675a277d80
GregHilston <Gregory.Hilston@gmail.com>
2019-07-01 18:42:35 -0600

    Slowly converting this to conways game of life, display i believe the domain object board

--- Challenges/challenge_42_conways_game_of_life/GregHilston/src/solution.py
+++ Challenges/challenge_42_conways_game_of_life/GregHilston/src/solution.py
@@ -1,80 +1,64 @@
-#!/usr/bin/env python
 import sys
 if sys.version_info[0] >= 3:
     import PySimpleGUI as sg
 else:
     import PySimpleGUI27 as sg
-import csv
+from enum import Enum
 
+class Cell(Enum):
+    DEAD = 0
+    ALIVE = 1
 
-def TableSimulation():
-    """
-    Display data in a table format
-    """
-    sg.SetOptions(element_padding=(0,0))
+class Table():
+    def __init__(self, rows=10, cols=10):
+        self.rows = rows
+        self.cols = cols
 
-    menu_def = [['File', ['Open', 'Save', 'Exit']],
-                ['Edit', ['Paste', ['Special', 'Normal',], 'Undo'],],
-                ['Help', 'About...'],]
+        self.board = []
+        for i in range(rows):
+            self.board.append([])
+            for j in range(cols):
+                self.board[i].append(Cell.DEAD)
 
-    columm_layout = [[]]
+class Simulator():
+    def __init__(self, table: Table):
+        self.table = table
 
-    MAX_ROWS = 20
-    MAX_COL = 10
-    for i in range(MAX_ROWS):
-        inputs = [sg.T('{}'.format(i), size=(4,1), justification='right')] + [sg.In(size=(10, 1), pad=(1, 1), justification='right', key=(i,j), do_not_clear=True) for j in range(MAX_COL)]
-        columm_layout.append(inputs)
+    def domain_model_to_ui(self):
+        ui = [[]]
 
-    layout = [ [sg.Menu(menu_def)],
-               [sg.T('Table Using Combos and Input Elements', font='Any 18')],
-               [sg.T('Type in a row, column and value. The form will update the values in realtime as you type'),
-               sg.In(key='inputrow', justification='right', size=(8,1), pad=(1,1), do_not_clear=True),
-               sg.In(key='inputcol', size=(8,1), pad=(1,1), justification='right', do_not_clear=True),
-               sg.In(key='value', size=(8,1), pad=(1,1), justification='right', do_not_clear=True)],
-               [sg.Column(columm_layout, size=(800,600), scrollable=True)] ]
+        for row in range(self.table.rows):
+            column = []
+            for col in range(self.table.cols):
+                column.append(
+                    sg.Text(
+                        size=(10, 1),
+                        pad=(1, 1),
+                        justification='right',
+                        key=(row,col),
+                        text=" ",
+                        background_color= "Black" if self.table.board[row][col] == Cell.DEAD else "White"
+                    )
+                )
+            inputs = [sg.T('{}'.format(row), size=(4,1), justification='right')] + column
+            ui.append(inputs)
+        print(str(type(ui)))
+        print(ui)
+        return ui
 
-    window = sg.Window('Table', return_keyboard_events=True).Layout(layout)
+    def start(self):
+        sg.SetOptions(element_padding=(0,0))
 
-    while True:
-        event, values = window.Read()
-        # --- Process buttons --- #
-        if event is None or event == 'Exit':
-            break
-        elif event == 'About...':
-            sg.Popup('Demo of table capabilities')
-        elif event == 'Open':
-            filename = sg.PopupGetFile('filename to open', no_window=True, file_types=(("CSV Files","*.csv"),))
-            # --- populate table with file contents --- #
-            if filename is not None:
-                with open(filename, "r") as infile:
-                    reader = csv.reader(infile)
-                    try:
-                        data = list(reader)  # read everything else into a list of rows
-                    except:
-                        sg.PopupError('Error reading file')
-                        continue
-                # clear the table
-                [window.FindElement((i,j)).Update('') for j in range(MAX_COL) for i in range(MAX_ROWS)]
+        columm_layout = self.domain_model_to_ui()
 
-                for i, row in enumerate(data):
-                    for j, item in enumerate(row):
-                        location = (i,j)
-                        try:            # try the best we can at reading and filling the table
-                            target_element = window.FindElement(location)
-                            new_value = item
-                            if target_element is not None and new_value != '':
-                                target_element.Update(new_value)
-                        except:
-                            pass
+        layout = [ [sg.T("Conway's Game of Life", font='Any 18')],
+                [sg.Column(columm_layout, size=(800,600), scrollable=True)] ]
 
-        # if a valid table location entered, change that location's value
-        try:
-            location = (int(values['inputrow']), int(values['inputcol']))
-            target_element = window.FindElement(location)
-            new_value = values['value']
-            if target_element is not None and new_value != '':
-                target_element.Update(new_value)
-        except:
-            pass
+        window = sg.Window('Table', return_keyboard_events=True).Layout(layout)
 
-TableSimulation()
+        while True:
+            event, values = window.Read()
+
+table = Table()
+simulator = Simulator(table)
+simulator.start()
