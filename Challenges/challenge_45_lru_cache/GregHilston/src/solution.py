38d82f6198551653a67da5e69bbce21e4e413996
GregHilston <Gregory.Hilston@gmail.com>
2019-07-22 13:01:22 -0600

    removed prints

--- Challenges/challenge_45_lru_cache/GregHilston/src/solution.py
+++ Challenges/challenge_45_lru_cache/GregHilston/src/solution.py
@@ -33,14 +33,11 @@ class LRUCache:
             Value found or -1 if not found
 
         """
-        print(f"Getting by key {key}")
         if key in self.cache:
             self.fake_time_stamp += 1
             self.cache[key][0] = self.fake_time_stamp
-            print(f"\tfound value {self.cache[key][1]}")
             return self.cache[key][1]
         else:
-            print(f"\tdid not find key {key}")
             return -1
 
     def put(self, key: int, value: int) -> None:
@@ -54,13 +51,11 @@ class LRUCache:
             None
 
         """
-        print(f"Putting key {key}, value {value}")
         self.fake_time_stamp += 1
         oldest_key = -1
         oldest_fake_time_stamp = sys.maxsize
 
         if len(self.cache.keys()) < self.capacity:
-            print(f"\t had room")
             self.cache[key] = [self.fake_time_stamp, value]
         else:
             for k, v in self.cache.items():
@@ -68,7 +63,6 @@ class LRUCache:
                     oldest_key = k
                     oldest_fake_time_stamp = v[0]
             # found who were replacing
-            print(f"\t making room by removing {oldest_key}")
             self.cache.pop(oldest_key)
             self.cache[key] = [self.fake_time_stamp, value]
         return None
