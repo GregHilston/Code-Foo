feefeb5841ab1ac1ca69f6aa1d04df3bc0fbe5f2
GregHilston <Gregory.Hilston@gmail.com>
2019-07-22 13:00:25 -0600

    added my solution with print outs

--- Challenges/challenge_45_lru_cache/GregHilston/src/solution.py
+++ Challenges/challenge_45_lru_cache/GregHilston/src/solution.py
@@ -1,5 +1,7 @@
 """Greg Hilston's source code solution to Code Foo challenge 45."""
 
+import sys
+
 from math import ceil
 
 from typing import Dict
@@ -18,7 +20,8 @@ class LRUCache:
 
         """
         self.capacity = capacity
-        self.cache: Dict[int, int] = {}
+        self.cache: Dict[int, [int, int]] = {}
+        self.fake_time_stamp = 0
 
     def get(self, key: int) -> int:
         """Fetch value by key.
@@ -30,7 +33,15 @@ class LRUCache:
             Value found or -1 if not found
 
         """
-        return -1
+        print(f"Getting by key {key}")
+        if key in self.cache:
+            self.fake_time_stamp += 1
+            self.cache[key][0] = self.fake_time_stamp
+            print(f"\tfound value {self.cache[key][1]}")
+            return self.cache[key][1]
+        else:
+            print(f"\tdid not find key {key}")
+            return -1
 
     def put(self, key: int, value: int) -> None:
         """Put value by key into cache.
@@ -43,4 +54,21 @@ class LRUCache:
             None
 
         """
+        print(f"Putting key {key}, value {value}")
+        self.fake_time_stamp += 1
+        oldest_key = -1
+        oldest_fake_time_stamp = sys.maxsize
+
+        if len(self.cache.keys()) < self.capacity:
+            print(f"\t had room")
+            self.cache[key] = [self.fake_time_stamp, value]
+        else:
+            for k, v in self.cache.items():
+                if v[0] < oldest_fake_time_stamp:
+                    oldest_key = k
+                    oldest_fake_time_stamp = v[0]
+            # found who were replacing
+            print(f"\t making room by removing {oldest_key}")
+            self.cache.pop(oldest_key)
+            self.cache[key] = [self.fake_time_stamp, value]
         return None
