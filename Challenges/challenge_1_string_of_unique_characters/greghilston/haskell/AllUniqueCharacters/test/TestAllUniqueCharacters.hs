12383c2df472861b7eae40c2274dc8414017de29
Jack Henahan <root@proofte.ch>
2019-07-25 15:41:16 -0400

    Fix indentation and module name

--- Challenges/challenge_1_string_of_unique_characters/greghilston/haskell/AllUniqueCharacters/test/TestAllUniqueCharacters.hs
+++ Challenges/challenge_1_string_of_unique_characters/greghilston/haskell/AllUniqueCharacters/test/TestAllUniqueCharacters.hs
@@ -1,34 +1,21 @@
-module TestAllUniqueCharacters where
-    import Test.HUnit
-    -- import AllUniqueCharacters
+module Main where
+import Test.HUnit
 
-    -- test= :: Test
-    -- test1 = TestCase assertBool "There are no duplicates" ("cat" [1])
+testSum = TestCase $ assertEqual "10 + 5 = 15" 15 (10 + 5)
 
+testProd = TestCase $ assertEqual "10 * 15" 150 (10 * 15)
 
+testPred = TestCase $ assertBool "10 > 5" (10 > 5)
 
-    -- tests :: Test
-    -- tests = TestList [TestLabel "test1" test1, TestLabel "test2" test2]
+testFailure = TestCase $ assertEqual "It will fail 10 + 2 = 15" (10 + 2) 15  
 
-    -- main :: IO Counts
-    -- main = do _ <- runTestTT tests
+testlist = TestList [TestLabel "testSum" testSum,
+                    TestLabel "testPred" testPred,
+                    TestLabel "testFailure" testFailure,
+                    TestLabel "testProd" testProd                    
+                    ]
 
-
-    testSum = TestCase $ assertEqual "10 + 5 = 15" 15 (10 + 5)
-
-    testProd = TestCase $ assertEqual "10 * 15" 150 (10 * 15)
-
-    testPred = TestCase $ assertBool "10 > 5" (10 > 5)
-
-    testFailure = TestCase $ assertEqual "It will fail 10 + 2 = 15" (10 + 2) 15  
-
-    testlist = TestList [TestLabel "testSum" testSum,
-                        TestLabel "testPred" testPred,
-                        TestLabel "testFailure" testFailure,
-                        TestLabel "testProd" testProd                    
-                        ]
-
-    main :: IO ()
-    main = do
-    runTestTT testlist
-    return ()
+main :: IO ()
+main = do
+  runTestTT testlist
+  return ()
