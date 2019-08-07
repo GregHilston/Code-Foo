70d37c432b70af46fdbafcfe5929cb37b81654d2
GregHilston <Gregory.Hilston@gmail.com>
2019-08-07 12:34:22 -0600

    Got the shell of Java solutions prepared. Now to write my implementation

---
+++ Challenges/challenge_46_can_place_flowers/greghilston/src/main/java/Solution.java
@@ -0,0 +1,5 @@
+public class Solution  {
+    public boolean solve(int[] flowerBed, int n) {
+        return false;
+    }
+ }
---
+++ Challenges/challenge_46_can_place_flowers/greghilston/src/test/java/TestSolution.java
@@ -0,0 +1,30 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.After;
+import org.junit.Test;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+public class TestSolution {
+    private Solution solution;
+
+    @Before
+    public void setUp() throws Exception {
+        solution = new Solution();
+    }
+
+    @Test
+    public void testOne()  {
+        boolean result = solution.solve(new int[] {1,0,0,0,1}, 1);
+
+        assertTrue("There should be room to plant one flower, dead center of the flower bed.", result);
+    }
+
+    @Test
+    public void testOne()  {
+        boolean result = solution.solve(new int[] {1,0,0,0,1}, 2);
+
+        assertFalse("There should not be room to plant two flowers. We only have room dead center.", result);
+    }
+}
