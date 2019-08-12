0719871d8313a219502f8c04dda5018f849db8e7
Dillon McElhinney <dillon.mce@gmail.com>
2019-08-12 13:15:46 -0600

    Solve challenge 48

---
+++ Challenges/challenge_48_highly_divisible_triangular_number/dillon-mce/highlyDivisibleTriangleNumbers.swift
@@ -0,0 +1,37 @@
+import Foundation
+extension Int {
+    func isDivisibleBy(_ number: Int) -> Bool {
+        return self % number == 0
+    }
+
+    var numberOfDivisors: Int {
+        var divisors = 0
+        let limit = Int(sqrt(Double(self))) + 1
+        for i in 1...limit {
+            guard self.isDivisibleBy(i) else { continue }
+            // add the current number and the other half
+            if self / i > limit {
+                divisors += 2
+            } else {
+                divisors += 1
+            }
+        }
+        return divisors
+    }
+}
+
+func findTriangleNumberWithDivisors(_ number: Int) -> Int {
+    var numberToCheck = 0
+    var increment = 0
+
+    while true {
+        increment += 1
+        numberToCheck += increment
+        let divisors = numberToCheck.numberOfDivisors
+        if divisors > number { return numberToCheck }
+    }
+}
+
+let startTime = CFAbsoluteTimeGetCurrent()
+print("The first number that meets the criteria is:  \(findTriangleNumberWithDivisors(500))")
+print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to calculate.")
