bc47d5261c11d1024b02e101fd4e467970a53205
Dillon McElhinney <dillon.mce@gmail.com>
2019-08-13 13:09:29 -0600

    Add calculation by prime factorialization.
    Cuts the calculation time down by about 25%

--- Challenges/challenge_48_highly_divisible_triangular_number/dillon-mce/highlyDivisibleTriangleNumbers.swift
+++ Challenges/challenge_48_highly_divisible_triangular_number/dillon-mce/highlyDivisibleTriangleNumbers.swift
@@ -15,6 +15,29 @@ extension Int {
         }
         return divisors
     }
+
+    func numDivisorsByPrimeFactorialization() -> Int {
+        var primeFactors: [Int: Int] = [:]
+
+        var n = self
+        while (n % 2 == 0) {
+            primeFactors[2, default: 1] += 1
+            n /= 2
+        }
+
+        let limit = Int(sqrt(Double(n))) + 1
+        var i = 3
+        while i < limit {
+            while (n % i == 0) {
+                primeFactors[i, default: 1] += 1
+                n /= i
+            }
+            i += 1
+        }
+        if n > 2 { primeFactors[n, default: 1] += 1}
+
+        return primeFactors.reduce(1) { $0 * $1.value }
+    }
 }
 
 func findTriangleNumberWithDivisors(_ number: Int) -> Int {
@@ -29,6 +52,22 @@ func findTriangleNumberWithDivisors(_ number: Int) -> Int {
     }
 }
 
-let startTime = CFAbsoluteTimeGetCurrent()
+func findTriangleNumberWithDivisorsByPrimes(_ number: Int) -> Int {
+    var numberToCheck = 0
+    var increment = 0
+
+    while true {
+        increment += 1
+        numberToCheck += increment
+        let divisors = numberToCheck.numDivisorsByPrimeFactorialization()
+        if divisors > number { return numberToCheck }
+    }
+}
+
+var startTime = CFAbsoluteTimeGetCurrent()
 print("The first number that meets the criteria is:  \(findTriangleNumberWithDivisors(500))")
-print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to calculate.")
+print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to calculate by brute force.")
+
+startTime = CFAbsoluteTimeGetCurrent()
+print("The first number that meets the criteria is:  \(findTriangleNumberWithDivisorsByPrimes(500))")
+print("It took \(CFAbsoluteTimeGetCurrent() - startTime) seconds to calculate by prime factorialization.")
